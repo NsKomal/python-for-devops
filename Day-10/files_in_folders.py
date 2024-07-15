3687e5d1453e1d0916d28d82bd32c0d4045e3385
NsKomal <86089772+NsKomal@users.noreply.github.com>
2024-07-15 06:50:10 +0000

    script completed

--- Day-10/files_in_folders.py
+++ Day-10/files_in_folders.py
@@ -1,2 +1,23 @@
-folders = input("pls entrt the folders: ")
-print(folders)
+import os 
+def files_in_folder(path):
+    try:
+        files = os.listdir(path)
+        return files, None
+    except FileNotFoundError:
+        return None, "Folder not found"
+ 
+
+def main ():   
+    folders = input("pls entrt the folders: ").split()
+    print(folders)
+
+    for folder in folders : 
+        print ("=========" + folder + "===========")
+        files, errormsg  = files_in_folder(folder)
+        if errormsg == None:
+            for file in files : 
+                print (file)
+        else :
+            print (folder + " : " + errormsg )
+if __name__ == "__main__":
+    main()
